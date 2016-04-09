585133d277e894a103215cc882de65e1e2b18017
John Janecek <janecektyler@gmail.com>
2016-04-08 21:55:34 -0700

    Deleted useless

--- plugins.py
+++
@@ -1,9 +0,0 @@
-from TriviaPlugin import TriviaPlugin
-from ExamplePlugin import ExamplePlugin
-
-def initialize_plugins() -> list:
-    # Plugins list. Append custom plugins here.
-    plugins = []
-    plugins.append(TriviaPlugin())
-    plugins.append(ExamplePlugin())
-    return plugins
