a304d03bf0e6f7adc426a6327664385d76c55e10
John Janecek <janecektyler@gmail.com>
2016-05-03 12:10:35 -0700

    Added 'safe shutdown' of gui if 'X' top left button is clicked

--- guis/BotGUI.py
+++ guis/BotGUI.py
@@ -7,14 +7,23 @@ class BotGUI():
     def __init__(self):
         self._root = Tk()
         self._root.wm_title("BotGUI")
+
+        # If x button clicked, call on_x_button
+        self._root.protocol('WM_DELETE_WINDOW', self.on_x_button)
+        self._x_button_clicked = False
+
+
         self._alive = True
 
+        # Black and white console
         self._text = Text(self._root, bg="black", fg="white")
         self._text.pack()
-        
+
+        # Exit button
         self._exit_button = Button(self._root, text = "EXIT STREAM", command = self.exit_stream)
         self._exit_button.pack()
-        
+
+        # Queue for messages to go into console
         self._queue = []
         self._lock = threading.Lock()
         
@@ -42,7 +51,11 @@ class BotGUI():
         self._lock.acquire()
         self._queue.append( str(msg) + sep )
         self._lock.release()
-        
+
+    def on_x_button(self):
+        # x button in corner was clicked
+        self.exit_stream()        
+
     def is_alive(self) -> bool:
         return self._alive
         
