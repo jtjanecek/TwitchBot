0ad637c39676ca099eaec9243ccc39da1b6cdb66
John Janecek <janecektyler@gmail.com>
2016-05-03 21:41:03 -0700

    Fixed bug when adding new plugins: sometimes would give error with Folder/Plugin name

--- plugins/plugins.py
+++ plugins/plugins.py
@@ -15,6 +15,11 @@ def initialize_plugins() -> list:
     for plugin in plugin_names:
         sys.path.insert(0, os.path.join(os.path.dirname(__file__),plugin))
         exec(("from " + plugin + " import " + plugin))
-        plugins.append(eval(plugin + "." + plugin + "()"))
+
+        # I don't know why but sometimes it works with the try
+        try:
+            plugins.append(eval(plugin + "." + plugin + "()"))
+        except (AttributeError):
+            plugins.append(eval(plugin + "()"))
 
     return plugins
